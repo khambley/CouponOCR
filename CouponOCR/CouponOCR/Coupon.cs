fc353f40a6c6405dc04f8f5928f0689fdb482d7e
Katherine Hambley <superdesigngirl@mac.com>
2022-04-11 15:15:26 -0500

    added coupon view and viewmodel files

---
+++ CouponOCR/CouponOCR/Coupon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponOCR
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string ManufacturerName { get; set; }
+        public decimal? SavingsAmount { get; set; }
+        public string StoreName { get; set; }
+        public string ProductName { get; set; }
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+        public string Photo { get; set; } = string.Empty;
+
+    }
+}
---
+++ CouponOCR/CouponOCR/CouponsViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CouponOCR
+{
+    public class CouponsViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Coupon> Coupons { get; } = new ObservableCollection<Coupon>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        bool busy;
+        public bool IsBusy
+        {
+            get { return busy; }
+            set
+            {
+                if (busy == value)
+                    return;
+
+                busy = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        public string Message { get; set; } = "Hello World";
+
+        Command addCouponCommand = null;
+
+        public Command AddCouponCommand => addCouponCommand ?? (addCouponCommand = new Command(async () => await ExecuteAddCouponCommandAsync()));
+
+        async Task ExecuteAddCouponCommandAsync()
+        {
+            try
+            {
+                IsBusy = true;
+                // 1. Add camera logic to take photo of coupon.
+
+
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
